771c068feedd415cc6d8afd7561c46ceae438e1f
nhatdaithienvuong <dotuanlam681@gmail.com>
2022-05-15 15:24:58 +0700

    Nop React ASM2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import TaskList from "./components/TaskList";
+import DatePicker from "./components/DatePicker";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      id:0,
+      levels: [
+        {
+          id: 0,
+          level: "Low",
+        },
+        {
+          id: 1,
+          level: "Medium",
+        },
+        {
+          id: 2,
+          level: "High",
+        },
+      ],
+      checked: "",
+      arrDeadlineName: [
+        {
+          idTask: 0,
+          nameTask: '',
+        },
+      ],
+      getColorLevel: "",
+    };
+  }
+
+  handleDelete=()=>{
+    this.setState(
+      {
+        arrDeadlineName: this.state.arrDeadlineName=this.state.arrDeadlineName.filter(e=>e!==e[0])
+      })
+  }
+
+  handleCheck = (check, colorLevel) => {
+    this.setState({
+      checked: (this.state.checked = check),
+      getColorLevel: (this.state.getColorLevel = colorLevel),
+    });
+  };
+  handleGetName = (e) => {
+    this.setState({
+      name: (this.state.name = e.target.value),
+    });
+  };
+
+  renderOnTaskList = () => {
+    this.refs.someName.value = "";
+    this.setState({
+      id:this.state.id = this.state.id +1,
+      arrDeadlineName: (this.state.arrDeadlineName = [
+        ...this.state.arrDeadlineName,
+        {
+          idTask: this.state.id  ,
+          nameTask: this.state.name === ''? null :  this.state.name ,
+        },
+      ]),
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="wrap">
+          <div className="new-task ">
+            <div className="container">
+              <div className="title">
+                <h1>New Task</h1>
+              </div>
+              <div className="form-input">
+                <div>
+                  <h3>Name</h3>
+                </div>
+                <input
+                  className="inputName"
+                  ref="someName"
+                  onChange={this.handleGetName}
+                  type="text"
+                  placeholder="Name task..."
+                />
+              </div>
+              <div className="deadline">
+                <div>
+                  <h3>Deadline</h3>
+                </div>
+              </div>
+              <div className="deadline-picker">
+                <DatePicker />
+              </div>
+              <div>
+                <h3>Priority</h3>
+              </div>
+              <div className="radio">
+                {this.state.levels.map((level) => (
+                  <div className="radio-container" key={level.id}>
+                    <input
+                      checked={level.id === this.state.checked}
+                      onChange={() => this.handleCheck(level.id, level.level)}
+                      id={level.id}
+                      type="radio"
+                    />
+                    <label className={level.level} htmlFor={level.id}>{level.level}</label>
+        
+                  </div>
+                ))}
+              </div>
+              <div className="btn-add">
+                <button className="btn-AddName" onClick={this.renderOnTaskList}>
+                  Add Task
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="container-list">
+            <TaskList
+             handleDelete={this.handleDelete}
+              iconLevel={this.state.getColorLevel}
+              deadlineNames={this.state.arrDeadlineName}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/TaskList.js
@@ -0,0 +1,41 @@
+import { Component } from "react";
+
+export default class TaskList extends Component {
+  houldComponentdidUpdate(nextProps, nextState) {
+    if (this.props.iconLevel !== nextProps) {
+      return true;
+    } else return false;
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="task-list ">
+        <div className="container-task-list">
+          <div className="title">
+            <h1> Task List</h1>
+          </div>
+          <div className="list-deadline">
+            {this.props.deadlineNames.map((deadlineName) => (
+              <div className="list" key={deadlineName.idTask}>
+                <button id="btn-icon" className={this.props.iconLevel} />
+                <h2>{deadlineName.nameTask}</h2>
+                <button
+                  onClick={this.props.handleDelete}
+                  className="btn-delete"
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
